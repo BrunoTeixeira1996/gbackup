8ef78665e48c2d990f4e5a6aa9f72fa3e74b85c8
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2023-07-27 16:36:56 +0100

    added goroutines for backups
    There still plumbing to be done in the log because for now it prints
    randomly

--- main.go
+++ main.go
@@ -1,56 +1,78 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"sync"
 
 	"github.com/BrunoTeixeira1996/gbackup/internal"
 	"github.com/BrunoTeixeira1996/gbackup/targets"
 )
 
+var supportedTargets = []string{
+	"leaks_backup",
+	"postgresql_backup",
+	"gokr_perm_backup",
+	"gokr_config_backup",
+	"syncthing_backup",
+	"monitoring_backup",
+}
+
+// Function that executes backup based on target type
+func getExecutionFunction(target string, cfg internal.Config) error {
+	switch target {
+	case "postgresql_backup":
+		if err := targets.ExecutePostgreSQLBackup(cfg); err != nil {
+			log.Println(err)
+		}
+
+	case "gokr_perm_backup":
+		if err := targets.ExecuteGokrPermBackup(cfg); err != nil {
+			log.Println(err)
+		}
+
+	case "gokr_config_backup":
+		if err := targets.ExecuteGokrConfBackup(cfg); err != nil {
+			log.Println(err)
+		}
+
+	case "syncthing_backup":
+		if err := targets.ExecuteSyncthingBackup(cfg); err != nil {
+			log.Println(err)
+		}
+
+	case "monitoring_backup":
+		if err := targets.ExecuteMonitoringBackup(cfg); err != nil {
+			log.Println(err)
+		}
+	case "leaks_backup":
+		if err := targets.ExecuteLeaksBackup(cfg); err != nil {
+			log.Println(err)
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	var (
 		cfg internal.Config
 		err error
+		wg  sync.WaitGroup
 	)
 
 	if cfg, err = internal.ReadTomlFile(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Starting Postgresql backup\n")
-	if err := targets.ExecutePostgreSQLBackup(cfg); err != nil {
-		log.Println(err)
+	for _, t := range supportedTargets {
+		wg.Add(1)
+		go func(t string) {
+			defer wg.Done()
+			log.Printf("Starting %s\n\n", t)
+			if err := getExecutionFunction(t, cfg); err != nil {
+				log.Println(err)
+			}
+		}(t)
 	}
-	fmt.Printf("-------------------------------\n")
-
-	fmt.Printf("Starting gokr perm partition backup\n")
-	if err := targets.ExecuteGokrPermBackup(cfg); err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("-------------------------------\n")
-
-	fmt.Printf("Starting Syncthing backup\n")
-	if err := targets.ExecuteSyncthingBackup(cfg); err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("-------------------------------\n")
-
-	fmt.Printf("Starting gokr config backup\n")
-	if err := targets.ExecuteGokrConfBackup(cfg); err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("-------------------------------\n")
-
-	fmt.Printf("Starting Leaks backup\n")
-	if err := targets.ExecuteLeaksBackup(cfg); err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("-------------------------------\n")
-
-	fmt.Printf("Starting monitoring backup\n")
-	if err := targets.ExecuteMonitoringBackup(cfg); err != nil {
-		log.Println(err)
-	}
-	fmt.Printf("-------------------------------\n")
+	wg.Wait()
 }
