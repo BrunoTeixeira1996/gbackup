f14b323e6ed0ffbff89d1173439bc78badbc97e6
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2023-10-17 15:42:18 +0100

    worklaptop: added new target

---
+++ targets/worklaptop.go
@@ -0,0 +1,45 @@
+package targets
+
+import (
+	"fmt"
+
+	"github.com/BrunoTeixeira1996/gbackup/internal"
+)
+
+func backupWorkLaptopToExternal(cfg internal.Config) error {
+	rCmd := []string{"-av", "--delete", "-e", "ssh", "worklaptop:{/home/brun0/Desktop/work,/home/brun0/Desktop/shared_folder}", "/mnt/pve/external/worklaptop_backup"}
+	if err := internal.ExecCmdToProm("rsync", rCmd, "rsync", cfg.Targets[6].Instance, cfg.Pushgateway.Host); err != nil {
+		return err
+	}
+	return nil
+}
+
+func backupWorkLaptopToHDD(cfg internal.Config) error {
+	c := []string{"-r", "/mnt/pve/external/worklaptop_backup/*", "/storagepool/backups/worklaptop_backup"}
+	err := internal.ExecCmdToProm("cp", c, "cmd", cfg.Targets[6].Instance, cfg.Pushgateway.Host)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function that handles both backups
+func ExecuteWorkLaptopBackup(cfg internal.Config) error {
+	isAlive, err := internal.IsAlive(cfg.Targets[6].MAC)
+	if err != nil {
+		return err
+	}
+	if isAlive {
+		if err := backupWorkLaptopToExternal(cfg); err != nil {
+			return err
+		}
+
+		if err := backupWorkLaptopToHDD(cfg); err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("The target %s is not alive: %w", cfg.Targets[6].Instance, err)
+	}
+	return nil
+}
