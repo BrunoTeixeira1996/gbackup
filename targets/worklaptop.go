de5db64b340673ef9635c0bc4201ea3d80fc19cb
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2024-09-13 22:53:10 +0100

    worklaptop: fixed next action that is backup worklaptop

--- targets/worklaptop.go
+++ targets/worklaptop.go
@@ -2,6 +2,7 @@ package targets
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/BrunoTeixeira1996/gbackup/internal/commands"
@@ -9,12 +10,21 @@ import (
 	"github.com/BrunoTeixeira1996/gbackup/internal/utils"
 )
 
+/*
+TODO: 
+backup ~/Desktop ~/.ssh /etc
+execute pipx list and save that for future (pipx list and save output)
+execute apt list installed and save that for future (dpkg --get-selections and save output)
+*/
 func backupWorkLaptopToExternal(cfg config.Config) error {
-	rCmd := []string{"-av", "-e", "ssh", "worklaptop:/home/brun0/Desktop/work", "worklaptop:/home/brun0/Desktop/shared_folder", "/mnt/pve/external/worklaptop_backup"}
-	if err := commands.ExecCmdToProm("rsync", rCmd, "toExternal", cfg.Targets[0].Instance, cfg.Pushgateway.Url); err != nil {
-		return err
+	command := "-av -e ssh worklaptop:/home/brun0/Desktop/work worklaptop:/home/brun0/Desktop/shared_folder /mnt/pve/external/worklaptop_backup"
 
+	log.Printf("[backup info] starting rsync command worklatop -> external\n")
+	if err := commands.RsyncCommand(command, "toExternal", "worklaptop", cfg.Pushgateway.Url); err != nil {
+		log.Printf("[backup error] could not perform RsyncCommand in worklaptop to external: %s\n", err)
+		return err
 	}
+	log.Printf("[backup info] completed backup of worklaptop to external\n")
 
 	return nil
 }
@@ -35,7 +45,7 @@ func ExecuteWorkLaptopBackup(cfg config.Config, el *utils.ElapsedTime) error {
 		el.Target = cfg.Targets[0].Name
 		el.Elapsed = end.Sub(start).Seconds()
 	} else {
-		return fmt.Errorf("[ERROR] the target %s is not alive: %w", cfg.Targets[0].Instance, err)
+		return fmt.Errorf("[backup error] the target %s is not alive: %w", cfg.Targets[0].Instance, err)
 	}
 	return nil
 }
