abaf73984ee933d17d3f3b6be38a1a001546f21b
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2024-09-13 22:20:41 +0100

    external: fixed rsync command to ignore some folders

--- targets/external.go
+++ targets/external.go
@@ -2,38 +2,29 @@ package targets
 
 import (
 	"log"
-	"time"
 
 	"github.com/BrunoTeixeira1996/gbackup/internal/commands"
 	"github.com/BrunoTeixeira1996/gbackup/internal/config"
-	"github.com/BrunoTeixeira1996/gbackup/internal/utils"
 )
 
 // Function that backups /external folder to NAS
-func ExecuteExternalToNASBackup(cfg config.Config, el *utils.ElapsedTime) error {
-	start := time.Now()
+func ExecuteExternalToNASBackup(cfg config.Config) error {
+	// TODO: calculate time
+	//start := time.Now()
 
-	/*
-		TODO:
-		exclude template
-		exclude snippets
-		exclude private
-		exclude lost+found
-		exclude images
-		exclude dump
-	*/
-	command := "-av --delete -e ssh /mnt/external  nas1:/mnt/datastore/backupExternal"
+	command := "-av --delete -e ssh --exclude=template --exclude=snippets --exclude=private --exclude=lost+found --exclude=images --exclude=dump /mnt/external nas1:/mnt/datastore/backupExternal"
 
-	log.Printf("[backup info] starting rsync command external -> NAS\n")
+	log.Printf("[backup info] starting rsync command external -> NAS (%s)\n", cfg.NAS.Name)
 	if err := commands.RsyncCommand(command, "toNAS", "external hard drive", cfg.Pushgateway.Url); err != nil {
 		log.Printf("[backup error] could not perform RsyncCommand in external to NAS: %s\n", err)
 		return err
 	}
+	log.Printf("[backup info] completed backup of external to NAS (%s)\n", cfg.NAS.Name)
 
 	// Calculate run time
-	end := time.Now()
-	el.Target = cfg.Targets[1].Name
-	el.Elapsed = end.Sub(start).Seconds()
+	// end := time.Now()
+	// el.Target = cfg.Targets[1].Name
+	// el.Elapsed = end.Sub(start).Seconds()
 
 	return nil
 }
