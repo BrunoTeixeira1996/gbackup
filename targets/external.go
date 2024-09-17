d12e7d0f79250abe7716c49ea55a44e2397e86ab
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2024-09-17 17:41:54 +0100

    external: added keepLastTwo to keep last two backups

--- targets/external.go
+++ targets/external.go
@@ -1,7 +1,10 @@
 package targets
 
 import (
+	"fmt"
 	"log"
+	"os/exec"
+	"regexp"
 
 	"github.com/BrunoTeixeira1996/gbackup/internal/commands"
 	"github.com/BrunoTeixeira1996/gbackup/internal/config"
@@ -13,16 +16,35 @@ type targets struct {
 	Command string
 }
 
-/*
-TODO:
-Implement function that keeps 2 backups and replace the older one with the new one
-*/
-func keepTwo() error {
-	/*
-	checks /mnt/datastore/backupExternal for folders and grab the folder names
-	validates the older date
-	delete that folder after the backup is executed in case of disaster during the backup
-	*/
+// Function that keeps the last two backups (newest)
+func keepLastTwo() error {
+	// list directories in /mnt/datastore/backupExternal
+	output, err := exec.Command("ssh", "nas1", "ls", "-la", "/mnt/datastore/backupExternal").Output()
+	if err != nil {
+		log.Printf("[keepLastTwo] error while listing: %s (%s)\n", output, err)
+		return err
+	}
+
+	// grab only folders using regex
+	regexPattern := `\d{4}-\d{2}-\d{2}`
+	re := regexp.MustCompile(regexPattern)
+
+	folderNames := re.FindAllString(string(output), -1)
+
+	// verify if there's at least 3 folders
+	if len(folderNames) < 3 {
+		log.Printf("[keepLastTwo] skipping this because there is only %d folder(s)\n", len(folderNames))
+		return nil
+	}
+
+	oldestFolder := fmt.Sprintf("/mnt/datastore/backupExternal/%s", folderNames[0])
+
+	output, err = exec.Command("ssh", "nas1", "rm -r", oldestFolder).Output()
+	if err != nil {
+		log.Printf("[keepLastTwo] error while deleting the oldest folder: %s (%s)\n", output, err)
+		return err
+	}
+	log.Printf("[keepLastTwo] successfully deleted oldest folder %s\n", oldestFolder)
 
 	return nil
 }
@@ -32,7 +54,6 @@ func ExecuteExternalToNASBackup(cfg config.Config) error {
 	// TODO: calculate time
 	//start := time.Now()
 
-	// TODO: i dont want to --delete the worklaptop target
 	t := []targets{
 		{
 			Name: "all (minus worklaptop)",
@@ -48,7 +69,7 @@ func ExecuteExternalToNASBackup(cfg config.Config) error {
 		},
 		{
 			Name:    "worklaptop",
-			Command: "-av -e ssh /mnt/external/worklaptop_backup nas1:/mnt/datastore/backupExternal/" + utils.CurrentTime() +"/external",
+			Command: "-av -e ssh /mnt/external/worklaptop_backup nas1:/mnt/datastore/backupExternal/" + utils.CurrentTime() + "/external",
 		},
 	}
 
@@ -60,6 +81,12 @@ func ExecuteExternalToNASBackup(cfg config.Config) error {
 		}
 	}
 	log.Printf("[backup info] completed backup of external to NAS (%s)\n", cfg.NAS.Name)
+
+	log.Printf("[backup info] verifying len of backup folders with keepLastTwo mechanism\n")
+	if err := keepLastTwo(); err != nil {
+		return err
+	}
+	log.Printf("[backup info] completed the house clean to keep 2 backup folders (newest)\n")
 
 	// Calculate run time
 	// end := time.Now()
